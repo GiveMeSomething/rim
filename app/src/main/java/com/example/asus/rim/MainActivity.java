40da00b445735809f66d66554a642c2fffeb94c3
Hoang Minh <minhbeo135@gmail.com>
2018-11-11 21:58:32 +0700

    add on, off button and status text to layout

--- app/src/main/java/com/example/asus/rim/MainActivity.java
+++ app/src/main/java/com/example/asus/rim/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.asus.rim;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = database.getReference();
+
+    final DatabaseReference light1status = databaseReference.child("light1").child("status");
+
+    Button onButton;
+    Button offButton;
+    TextView status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        onButton = findViewById(R.id.onButton);
+        offButton = findViewById(R.id.offButton);
+
+        status = findViewById(R.id.status);
     }
 }
