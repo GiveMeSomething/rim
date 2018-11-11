c846a2ccda07b1b49f784460c9158a49b2a95e14
Hoang Minh <minhbeo135@gmail.com>
2018-11-11 22:02:51 +0700

    add event listener for buttons to make change in real-time database

--- app/src/main/java/com/example/asus/rim/MainActivity.java
+++ app/src/main/java/com/example/asus/rim/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.asus.rim;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -45,6 +46,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 Toast.makeText(getApplicationContext(), "Failed to fetch data", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                light1status.setValue("ON");
+            }
+        });
+
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                light1status.setValue("OFF");
             }
         });
     }
