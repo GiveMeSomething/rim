b5456c7c5af59de2659da626e34f8c3572e04229
Hoang Minh <minhbeo135@gmail.com>
2018-12-18 23:06:11 +0700

    create a class to process AIResponse

---
+++ app/src/main/java/com/example/asus/rim/AIResponseProcessor.java
@@ -0,0 +1,33 @@
+package com.example.asus.rim;
+
+import com.google.gson.JsonElement;
+
+import java.util.HashMap;
+
+import ai.api.model.AIResponse;
+import ai.api.model.Metadata;
+import ai.api.model.Result;
+
+public class AIResponseProcessor {
+    private AIResponse aiResponse;
+    private String section;
+    private String usage;
+    private String targetObject;
+    private String[] keyPackage = {"Applications"};
+
+    public AIResponseProcessor(AIResponse aiResponse) {
+        this.aiResponse = aiResponse;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public String getTargetObject() {
+        return targetObject;
+    }
+}
