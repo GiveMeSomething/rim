94881d46010236097d1679615c310f5ada622258
Hoang Minh <minhbeo135@gmail.com>
2018-12-21 00:00:29 +0700

    delete unused import

--- app/src/main/java/com/example/asus/rim/MainActivity.java
+++ app/src/main/java/com/example/asus/rim/MainActivity.java
@@ -6,18 +6,14 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import ai.api.AIConfiguration;
 import ai.api.AIServiceException;
